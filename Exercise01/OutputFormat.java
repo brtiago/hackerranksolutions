023b9fc48bf2f203752c55ed17c78e1c3d9dc796
brtiago <tgribeiro@gmail.com>
2024-06-30 04:09:25 -0300

    Exrcise 01

---
+++ Exercise01/OutputFormat.java
@@ -0,0 +1,11 @@
+package Exercise01;
+
+public class OutputFormat {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        System.out.println("Hello, World.");
+        System.out.println("Hello, Java.");
+
+    }
+}
