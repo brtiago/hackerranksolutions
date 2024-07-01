94487e325228c921651cf20d194c64b4f50769a9
brtiago <tgribeiro@gmail.com>
2024-07-01 17:34:03 -0300

    Solução Exercise07

---
+++ Java/Exercise07/Exercise07.java
@@ -0,0 +1,25 @@
+package Java.Exercise07;
+
+import java.io.*;
+import java.util.*;
+
+public class Exercise07 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String a = sc.next();
+        String b = sc.next();
+
+        sc.close();
+        System.out.println(a.length() + b.length());
+        System.out.println(a.compareTo(b) > 0 ? "Yes" : "No");
+        System.out.println(capitalize(a) + " " + capitalize(b));
+    }
+
+    private static String capitalize(String str){
+        if (str == null || str.isEmpty()){
+            return str;
+        }
+        return str.substring(0,1).toUpperCase()+str.substring(1);
+    }
+}
