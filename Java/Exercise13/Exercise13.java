8eef04839bbb3d944d72fb616b3c3d2715486574
brtiago <tgribeiro@gmail.com>
2024-07-14 08:53:19 -0300

    Solução Exercise13

---
+++ Java/Exercise13/Exercise13.java
@@ -0,0 +1,20 @@
+package Java.Exercise13;
+
+import java.util.Scanner;
+
+public class Exercise13 {
+
+    public static void main(String[] args) {
+        try(Scanner in = new Scanner(System.in)) {
+            while(in.hasNext()) {
+                String ip = in.nextLine();
+                System.out.println(ip.matches(MyRegex.pattern));
+            }
+
+        }
+    }
+
+    class MyRegex {
+        public static final String pattern = "^((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$";
+    }
+}
