58897a8ab3aa608608c1acdbe5924f67d98675d2
brtiago <tgribeiro@gmail.com>
2024-08-05 04:40:09 -0300

    Hackerrank tag content extractor solution

---
+++ Java/tag_content_extractor/Solution.java
@@ -0,0 +1,16 @@
+package Java.tag_content_extractor;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Solution {
+
+    public static void main(String[] args) throws IOException{
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(in.readLine().trim());
+
+    }
+    
+}
