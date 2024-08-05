daf758e724166aab64d512bc90d2501d49e67807
brtiago <tgribeiro@gmail.com>
2024-08-05 04:40:37 -0300

    Hackerrank largest prime factor solution

---
+++ ProjectEuler/Problem_3_largest_prime_factor/Solution.java
@@ -0,0 +1,20 @@
+package ProjectEuler.Problem_3_largest_prime_factor;
+
+public class Solution {
+
+    public static void main(String[] args) {
+
+        System.out.println("Largest prime factor: " + largestPrimeFactor(3) + " // should return 3");
+        System.out.println("Largest prime factor: " + largestPrimeFactor(5) + " // should return 5");
+        System.out.println("Largest prime factor: " + largestPrimeFactor(6) + " // should return 7");
+        System.out.println("Largest prime factor: " + largestPrimeFactor(8) + " // should return 2");
+        System.out.println("Largest prime factor: " + largestPrimeFactor(13195) + " // should return 29");
+        //System.out.println("Largest prime factor: " + largestPrimeFactor(600851475143) + " // should return 6857");
+    }
+
+    private static int largestPrimeFactor(int number) {
+        int sum = 0;
+        
+        return sum;
+    }
+}
